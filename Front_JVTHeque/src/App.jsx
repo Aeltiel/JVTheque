c97b5b6b97c48d6b507c5dd3183098c64d72422b
Aeltiel <ingrid.stemer@live.fr>
2024-03-01 18:42:30 +0100

    add tailwind into project

--- Front_JVTHeque/src/App.jsx
+++ Front_JVTHeque/src/App.jsx
@@ -1,24 +1,31 @@
-import {Routes, Route} from 'react-router-dom';
-import './Styles/main.scss';
-import AuthRoute from './Authentification/AuthRoute';
-import Header from './Component/Layout/Header';
-import Footer from './Component/Layout/Footer';
-import Home from './Page/Home';
-import UserPage from './Page/UserPage';
-
+import { Routes, Route } from "react-router-dom";
+import "./Styles/main.scss";
+import "./Styles/dist/output.css";
+import AuthRoute from "./Authentification/AuthRoute";
+import Header from "./Component/Layout/Header";
+import Footer from "./Component/Layout/Footer";
+import Home from "./Page/Home";
+import UserPage from "./Page/UserPage";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path="/" element={<Home />} />
         {/* ligne 16 : utiliser le component AuthRoute dans "element" pour géré l'authentification
         et éviter l'erreur de route : Uncaught Error: [AuthRoute] is not a <Route> component. 
         All component children of <Routes> must be a <Route> or <React.Fragment> */}
-        <Route path="/userPage" element={<AuthRoute><UserPage/></AuthRoute>}/>
+        <Route
+          path="/userPage"
+          element={
+            <AuthRoute>
+              <UserPage />
+            </AuthRoute>
+          }
+        />
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
